9b0cf2a69096188c2c42225cd3e357c64ab6b4cf
Danh Nguyen Van <danh.nguyenvan@hermes-europe.co.uk>
2025-02-18 17:05:18 +0700

    Added product carousel, Bootstrap 5.3.2 & custom font

--- PhoneShopClient/Program.cs
+++ PhoneShopClient/Program.cs
@@ -3,11 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PhoneXpressClient;
 using PhoneXpressClient.Services;
 using PhoneXpressSharedLibrary.Abstracts;
+using Syncfusion.Blazor;
+
+//Register Syncfusion license
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzcyMzU5N0AzMjM4MmUzMDJlMzBaR3NDVWp5dHB4NldjenFtVnJFdHlsSkFWaTNsdmxnTGZvanl3c2hTTFlJPQ==");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProduct, ClientServices>();
 await builder.Build().RunAsync();
+
